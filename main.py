ec4513474275a545c6996bf6ca199a06eca5b35a
WerWolv <werwolv98@gmail.com>
2022-04-13 22:07:15 +0200

    Updated plugin to support latest loader changes

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import subprocess
 
 class Plugin:
-    name = "Extra Settings"
-
-    author = "WerWolv"
-
-    main_view_html = "index.html"
-
-    tile_view_html = ""
-
     async def get_ssh_state(self):
         return subprocess.Popen("systemctl is-active sshd", stdout=subprocess.PIPE, shell=True).communicate()[0] == b'active\n'
 
-    async def set_ssh_state(self, **kwargs):
-        print(kwargs["state"])
-
-        if kwargs["state"]:
-            print(subprocess.Popen("systemctl start sshd", stdout=subprocess.PIPE, shell=True).communicate())
+    async def set_ssh_state(self, state):
+        if state:
+            subprocess.Popen("systemctl enable --now sshd", stdout=subprocess.PIPE, shell=True).wait()
         else:
-            print(subprocess.Popen("systemctl stop sshd", stdout=subprocess.PIPE, shell=True).communicate())
+            subprocess.Popen("systemctl disable --now sshd", stdout=subprocess.PIPE, shell=True).wait()
         
         return await self.get_ssh_state(self)
