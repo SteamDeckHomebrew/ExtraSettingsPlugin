cf9cadee23dfc3905486a8c8b872d7eb69920216
NGnius (Graham) <ngniusness@gmail.com>
2022-05-19 03:45:05 -0400

    Add Transparent Huge Pages toggle (#4)
    * Add Transparent Huge Pages toggle
    
    * Update to latest CSS class names

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This is a plugin for the [SteamOS Plugin Loader](https://github.com/SteamDeckHom
 ## Features
 
 - Toggling the SSH server on and off
+- Toggling Transparent Huge Pages on and off, which [can improve gaming performance](https://www.reddit.com/r/linux_gaming/comments/uhfjyt/underrated_advice_for_improving_gaming/).
--- main.py
+++ main.py
@@ -9,5 +9,20 @@ class Plugin:
             subprocess.Popen("systemctl enable --now sshd", stdout=subprocess.PIPE, shell=True).wait()
         else:
             subprocess.Popen("systemctl disable --now sshd", stdout=subprocess.PIPE, shell=True).wait()
-        
+
         return await self.get_ssh_state(self)
+
+    async def set_thp_state(self, state):
+        if state:
+            with open("/sys/kernel/mm/transparent_hugepage/enabled", "w") as f:
+                f.write("always")
+        else:
+            with open("/sys/kernel/mm/transparent_hugepage/enabled", "w") as f:
+                f.write("madvise")
+
+        return await self.get_thp_state(self)
+
+
+    async def get_thp_state(self):
+        with open("/sys/kernel/mm/transparent_hugepage/enabled", "r") as f:
+            return f.read().strip().startswith("[always]")
